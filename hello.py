0efebf17ffbdee6acffa1c2f81ed20582e406fd3
sunabove <sunabove@nate.com>
2018-10-06 12:14:05 +0900

    **

--- hello.py
+++ hello.py
@@ -5,14 +5,32 @@ logging.basicConfig(
     )
 
 def sum_between_two_number( x, y ) : 
+    # Create a workbook and add a worksheet.
+    import xlsxwriter
+    workbook = xlsxwriter.Workbook('sum_data.xlsx')
+    worksheet = workbook.add_worksheet()
+
     t = x
     x = min( t, y )
     y = max( t, y ) 
     sum = 0
+    row = 0 
+    worksheet.write(row, 0, "x" )
+    worksheet.write(row, 1, "sum" )
+    row += 1
+
     for x in range( x, y + 1 ):
         sum = sum + x 
+        worksheet.write(row, 0, x )
+        worksheet.write(row, 1, sum )
+        row += 1
         logging.debug( " current x = %s, sum = %s" % ( x, sum ) )
     pass
+
+    worksheet.write(row, 0, "sum" )
+    worksheet.write(row, 1, sum ) 
+
+    workbook.close()
 
     return sum
 pass
