0b353bb988c07d567c37a7a1a88d4bac6456e7f3
sunabove <sunabove@nate.com>
2018-10-13 12:08:49 +0900

    **

--- pythagoras.py
+++ pythagoras.py
@@ -1,15 +1,37 @@
 import math
 
-def find_pythagoras():
-    for x in range( 1, 101 ): 
-        for y in range( 1, 101 ):  
-            real_z = math.sqrt( x*x + y*y ) 
-            z = int( real_z )
-            if z == real_z and z < 101 : 
-                print( "x = %s, y= %s, z= %s" %(x, y, z) )
+class Pythagoras :
+
+    def find_numbers_analytic( self ):
+        print( "find_numbers_analytic" )
+        for x in range( 1, 101 ): 
+            for y in range( x + 1, 101 ):  
+                real_z = math.sqrt( x*x + y*y ) 
+                z = int( real_z )
+                if z == real_z and z < 101 : 
+                    print( "x = %s, y= %s, z= %s" %(x, y, z) )
+                pass
+            pass
+        pass
+    pass
+
+    def find_numbers_numerical( self ):
+        print( "find_numbers_numerical" )
+        for x in range( 1, 101 ) : 
+            for y in range( x + 1, 101 ) :  
+                for z in range( y , 100 ) :
+                    if x*x + y*y == z*z : 
+                        print( "x = %s, y= %s, z= %s" %(x, y, z) )
+                    pass
+                pass
             pass
         pass
     pass
 pass
 
-find_pythagoras()
+p = Pythagoras()
+p.find_numbers_numerical()
+
+#Pythagoras.find_numbers_analytic( p )
+
+#--
