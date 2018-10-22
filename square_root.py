641dc92bb97bba439af4ac39604b52825e9c514f
sunabove <sunabove@nate.com>
2018-10-22 15:07:20 +0900

    **

--- square_root.py
+++ square_root.py
@@ -1,11 +1,14 @@
 import math
+
+import logging
+logging.basicConfig( format='%(levelname)-8s %(asctime)s %(filename)s %(lineno)d %(message)s', level=logging.DEBUG )
 class SquareRoot :
     def find_square_root(self, x ) :
         e = 0.000001 
         y = e 
 
         while abs( y*y - x ) > e :
-            y += e 
+            y += e
             #print( "y = %s" % y )
         pass
         
---
+++ web_scape_example.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+class What :
+
+    def __init__(self)
+        pass
+    pass
+
+    def doSomething(self):
+        result = None 
+        return result 
+    pass
+
+pass
+
+if __name__ == '__main__' :   
+    what = What()
+    what.doSomething()
+pass
