44ed5da1c0418ecc7ca1c664f4be3638db008ea3
sunabove <sunabove@nate.com>
2018-10-29 06:23:09 +0900

    **

---
+++ ppt_exam_04.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+print( "Hello..." )
+
+from pptx import Presentation
+from pptx.util import Inches
+
+import shutil
+import requests
+
+# 이미지 가져와서 저장하기
+my_url = 'https://www.washingtonian.com/wp-content/uploads/2017/06/6-30-17-goat-yoga-congressional-cemetery-1-994x559.jpg'
+response = requests.get(my_url, stream=True)
+with open( 'my_image.png', 'wb') as file:
+    shutil.copyfileobj(response.raw, file) 
+pass
+
+img_path = 'my_image.png'
+
+prs = Presentation()
+blank_slide_layout = prs.slide_layouts[6]
+slide = prs.slides.add_slide(blank_slide_layout)
+
+left = top = Inches(1) 
+height = Inches(5.5)
+pic = slide.shapes.add_picture(img_path, left, top, height=height)
+
+prs.save('test.pptx')
+
+print( "Good bye!")
