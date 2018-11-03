0409fe19a2a30e682d02ebf83aaa52cc4a7d24d1
sunabove <sunabove@nate.com>
2018-11-03 12:48:22 +0900

    **

--- ppt_report.py
+++ ppt_report.py
@@ -1,26 +1,21 @@
 # coding=utf-8
-
 import logging
 logging.basicConfig( format='%(levelname)-8s %(asctime)s %(filename)s %(lineno)d %(message)s', level=logging.DEBUG )
 
 class Report :
-
     def __init__(self) :
         pass
     pass
 
     def makeReport(self) :  
-
         line = "\n" + "#"*80 + "\n"
 
         # import libraries
         from bs4 import BeautifulSoup
-
         # specify the url
         html_url = "https://ko.wikipedia.org/wiki/%EA%B3%A0%EB%A0%A4%EC%9D%B8"
 
         # query the website and return the html to the variable ‘page’
-
         logging.info( "Getting a html data from %s" % html_url )
 
         from urllib.request import urlopen
@@ -65,13 +60,34 @@ class Report :
         tf.text = html.h1.string
         tf.paragraphs[0].font.size = Pt(18)
 
-        div = html.find( "div", {"class": "mw-parser-output" } )
+        div = html.find( "div", { "class": "mw-parser-output" } )
 
         pList = div.find_all( "p" ) 
 
         for i in range( 2, 5 ) :
             p = tf.add_paragraph()
             p.text = pList[ i ].getText().strip()
+            p.level = 1 
+            p.font.size = Pt(14)
+        pass
+
+        # 세번째 목차 PPT 슬라이드 추가 
+        bullet_slide_layout = prs.slide_layouts[1]
+
+        slide = prs.slides.add_slide(bullet_slide_layout)
+        shapes = slide.shapes
+
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
+        title_shape.text = "목차"
+
+        tf = body_shape.text_frame
+        div = html.find( "div", {"id": "toc" , "class": "toc" } )
+        liList = div.find( "ul" ).find_all( "li" ) 
+
+        for li in liList :
+            p = tf.add_paragraph()
+            p.text = li.getText().strip()
             p.level = 1 
             p.font.size = Pt(14)
         pass 
