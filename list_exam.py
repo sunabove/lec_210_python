3cf5838e705cf6ba280b83245b5956fe7356f542
sunabove <sunabove@nate.com>
2018-12-01 13:15:21 +0900

    **

---
+++ list_exam.py
@@ -0,0 +1,12 @@
+a =  [ 'foo', 'bar', 'baz', 'qux', 'quux', 'corge' ]
+print( a ) 
+b = a[ : : -1 ]
+print( b ) 
+b = a[ 6 : : -1 ]
+print( b )  
+b = a.reverse()
+print( a ) 
+print( "-"*40 ) 
+print( b ) 
+
+print( "---------------------------------------------------" ) 
---
+++ tuple_exam.py
@@ -0,0 +1,9 @@
+days = ( "일", "월", "화", "수", "목", "금", "토" )
+holidays = ( "일", "월" )
+
+print( days )
+print( holidays )
+
+for day in days :
+    print( day )
+pass
